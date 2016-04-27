f171f3b203c82a2c7d03e39b753f99db3836b196
Rooopeswar Kommalapati <roopeshkom@gmail.com>
2016-04-27 02:08:00 -0400

    Fixed some typos and bugs

--- app/run.py
+++ app/run.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from flask import render_template
-from bs4 import BeautifulSoup
-import urllib2
+# from bs4 import BeautifulSoup
+# import urllib2
 
 app = Flask(__name__)
-# html = urllib2.urlopen('https://www.google.com').read()
-# soup = BeautifulSoup(html, 'html.parser')
-
 
 @app.route('/')
 @app.route("/index")
 def hello():
 	lis = [1,1241,4,453,-45, 45,2,23]
-	lis = sorted(lis, reverse = True)
-	return render_template("index.html", lis = lis, url = "https://www.facebook.com")
+	return render_template("index.html", lis = sorted(lis, reverse=True), url = "https://www.facebook.com")
 
 app.run(debug = True)
