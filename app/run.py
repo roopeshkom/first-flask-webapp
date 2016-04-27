d70a1522e31c7a7c6f45f754033ea0c4e670fe34
Rooopeswar Kommalapati <roopeshkom@gmail.com>
2016-04-27 02:56:14 -0400

    Added links and 404 page

--- app/run.py
+++ app/run.py
@@ -6,9 +6,15 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route("/index")
+@app.route('/index')
 def hello():
 	lis = [1,1241,4,453,-45, 45,2,23]
-	return render_template("index.html", lis = sorted(lis, reverse=True), url = "https://www.facebook.com")
+	return render_template("index.html", lis = sorted(lis, reverse=True))
+
+@app.route('/cool-stuff')
+@app.route('/about-me')
+@app.route('/secrets')
+def ohno():
+	return render_template("404.html", lol = range(13))
 
 app.run(debug = True)
